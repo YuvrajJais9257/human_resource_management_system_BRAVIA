ff7155748a3dd7db83baea394348361db067eddd
Yuvraj Jaiswal <quicksilver92571331@gmail.com>
2026-02-07 15:40:48 +0530

    updated db.py for deployment purposes

--- backend/app/db.py
+++ backend/app/db.py
@@ -22,6 +22,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
 if not all([DB_USER, DB_PASS, DB_HOST, DB_NAME]):
@@ -31,7 +32,7 @@ if not all([DB_USER, DB_PASS, DB_HOST, DB_NAME]):
 
 # ================= DATABASE URL =================
 
-DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+DB_URL =f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 # ================= ENGINE =================
